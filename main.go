55bce126792f110470d2143e2b3cfec216ba302f
hitaki <m.hitaki@gmail.com>
2016-12-14 13:57:57 +0900

    リファクタリング

--- main.go
+++ main.go
@@ -1,51 +1,79 @@
+//******************************************************************************
+//	ヒットアンドブロー
+//
+//	-comment-
+//	GO言語の練習用に作ったやつ
+//
+//	-note-
+//	2016/12/13	新規作成
+//******************************************************************************
 package main
 
+//******************************************************************************
 //	インポート
-import "fmt"
-import "unicode/utf8"
-import "errors"
+//******************************************************************************
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+)
 
+//******************************************************************************
 //	定数
+//******************************************************************************
 const (
 	InputLen = 4 //	入力文字列の長さ
 )
 
+//******************************************************************************
 //	メイン
+//******************************************************************************
 func main() {
 
+	//
+	//	タイトル表示
+	//
 	viewTitle()
 
-	var src string
+	var inputString string
 	var i int
 
+	//
 	//	メインループ
+	//
 	for i = 0; ; {
+
+		var inputData []int
 
 		fmt.Printf("【%d回目】%d桁の数字を入れてね > ", (i + 1), InputLen)
 
-		fmt.Scanln(&src)
-		fmt.Println("入力>" + src)
+		fmt.Scanln(&inputString)
+		//src = "1234"
+		fmt.Println("入力>" + inputString)
 
-		if err := validateSourceText(src); nil != err {
+		//	入力文字列検証
+		inputData, err := validateSourceText(inputString)
 
+		if nil != err {
 			fmt.Println(err.Error())
 			fmt.Println("")
-
 			continue
-
 		} else {
-
 			fmt.Println("いけたね")
+			fmt.Println(inputData)
 			i++
-
 		}
 
 		break
 	}
 }
 
+//******************************************************************************
 //	タイトル表示
+//******************************************************************************
 func viewTitle() {
+
 	fmt.Println("")
 	fmt.Println("////////////////////////////////")
 	fmt.Println("  Hit and Blow!")
@@ -53,17 +81,45 @@ func viewTitle() {
 	fmt.Println("")
 }
 
+//******************************************************************************
 //	入力文字検証
-func validateSourceText(i_Src string) (err error) {
+//******************************************************************************
+func validateSourceText(iSrc string) (inputData []int, err error) {
 
+	//
 	//	文字数チェック
-	l := utf8.RuneCountInString(i_Src)
+	//
+	srcLen := utf8.RuneCountInString(iSrc)
 
-	if (0 >= l) || (InputLen <= l) {
+	if 0 >= srcLen {
+		err = errors.New("入力が正しくない")
+		return
+	} else if InputLen < srcLen {
 		err = errors.New("入力文字数が多い")
+		return
 	}
 
-	//var err error
+	//
+	//	すべての文字が数字かチェック＆戻りの配列に値をセット
+	//
+	inputData = make([]int, InputLen)
+
+	for i := 0; i < srcLen; i++ {
+
+		var char = iSrc[i:(i + 1)]
+		var work int
+		var errWork error
+
+		//	数字へ変換
+		work, errWork = strconv.Atoi(char)
+
+		if nil == errWork {
+			inputData[i] = work
+		} else {
+			err = errors.New("数字以外が含まれている (" + char + ")")
+			return
+		}
+	}
 
 	//err = errors.New("ほげー")
 
